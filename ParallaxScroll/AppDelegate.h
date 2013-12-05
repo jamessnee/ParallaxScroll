23524c2b7c7a432b13bfe60c08024314c7da9671
James Snee <james.snee@gmail.com>
2013-12-05 14:21:17 +0000

    Initial Commit

---
+++ ParallaxScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParallaxScroll
+//
+//  Created by James Snee on 05/12/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
